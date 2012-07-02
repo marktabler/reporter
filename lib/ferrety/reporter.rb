35b36c075cbf3a3acf7912912733889998d4ddaa
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-02 11:29:04 -0400

    set reporter endpoint to production

--- lib/ferrety/reporter.rb
+++ lib/ferrety/reporter.rb
@@ -3,7 +3,7 @@ require 'json'
 require "resque"
 
 INTERNAL_PASSWORD = '350c9d803c149399e61641e1e81228464f94e02351afb18da921096f7d6e9caee1722560db2000e73851699c8fd8d869d604ec91d49b6982483cc6960a5a4d82'
-REPORTER_ENDPOINT = 'http://localhost:3000/reports/'
+REPORTER_ENDPOINT = 'http://ferrety.net/reports/'
 
 module Ferrety
 
