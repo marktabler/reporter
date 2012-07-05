92010d91b86b5d9c25cf62cc28b5d24847e45b8b
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-05 10:10:58 -0400

    Extract password to environment variable

--- lib/ferrety/reporter.rb
+++ lib/ferrety/reporter.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'json'
 require "resque"
 
-INTERNAL_PASSWORD = '350c9d803c149399e61641e1e81228464f94e02351afb18da921096f7d6e9caee1722560db2000e73851699c8fd8d869d604ec91d49b6982483cc6960a5a4d82'
+INTERNAL_PASSWORD = ENV['FERRETY_PASSWORD']
 REPORTER_ENDPOINT = 'http://ferrety.net/reports/'
 
 module Ferrety
